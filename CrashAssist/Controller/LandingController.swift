4495dd5947c42600e6327ddc914de253598276e4
Netanel Sadeghi <nnsadegh@usc.edu>
2023-05-02 19:28:21 -0700

    Fixed bug where user would get prompted to enter more info even after having created an account before

--- CrashAssist/Controller/LandingController.swift
+++ CrashAssist/Controller/LandingController.swift
@@ -67,28 +67,30 @@ extension BaseViewController: FUIAuthDelegate {
             return
         }
         
-        // Check if the user has filled in their Vin already (meaning they are not signing up but logging in)
-        if (UserManager.shared.getCurrentUser()?.vin != nil) {
-            // User Document exists
-            // User is logging in
-            // Don't ask for more information and go to home page
-            self.performSegue(withIdentifier: "goHomeFromLogin", sender: self)
-        } else {
-            // User Document does not exist
-            // User is signing up
-            // Transition to more page for more info
-            let currUser = Auth.auth().currentUser!
-            let data: [UserManager.UserField: Any] = [
-                UserManager.UserField.name: currUser.displayName!,
-                UserManager.UserField.email: currUser.email!
-            ]
-            let user: User = User(userID: currUser.uid, data: data)
-            UserManager.shared.updateUser(user: user) { error in
-                if let error = error {
-                    // Handle the error
-                    print("Error updating user data: \(error)")
-                } else {
-                    self.performSegue(withIdentifier: "goMoreFromLogin", sender: self)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            // Check if the user has filled in their Vin already (meaning they are not signing up but logging in)
+            if (UserManager.shared.getCurrentUser()?.vin != nil) {
+                // User Document exists
+                // User is logging in
+                // Don't ask for more information and go to home page
+                self.performSegue(withIdentifier: "goHomeFromLogin", sender: self)
+            } else {
+                // User Document does not exist
+                // User is signing up
+                // Transition to more page for more info
+                let currUser = Auth.auth().currentUser!
+                let data: [UserManager.UserField: Any] = [
+                    UserManager.UserField.name: currUser.displayName!,
+                    UserManager.UserField.email: currUser.email!
+                ]
+                let user: User = User(userID: currUser.uid, data: data)
+                UserManager.shared.updateUser(user: user) { error in
+                    if let error = error {
+                        // Handle the error
+                        print("Error updating user data: \(error)")
+                    } else {
+                        self.performSegue(withIdentifier: "goMoreFromLogin", sender: self)
+                    }
                 }
             }
         }
